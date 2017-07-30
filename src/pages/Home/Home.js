cafc143519e1885e72736394e900ecf2fac81c59
Nessa <nessa.develops@gmail.com>
2017-07-29 19:15:04 -0500

    Fonts fix, home css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,26 +6,11 @@ import '../../styles/theme.css';
 class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-        </div>
-        <img src={logo} className="home-logo" alt="logo" />
-        <div>
-          <h1>NESSA STABLER</h1>
-          <div className="subtitle">WEB DESIGNER & DEVELOPER</div>
-          <div className="hobbies" id="rotate">
-            <div>Avid Hiker</div>
-            <div>Devoted Dog Mama</div>
-            <div>Passionate Video Gamer</div>
-            <div>Meme Enthusiast</div>
-            <div>Aspiring Globe Trotter</div>
-          </div>
-          <nav className="nav row">
-            <a href="/about">About</a>
-            <a href="/projects">Projects</a>
-            <a href="/contact">Contact</a>
-          </nav>
-        </div>
+      <div className="App Montserrat">
+        <p>hi my name is</p>
+        <h1 className="MontserratBold">nessa.</h1>
+        <p>i&#39;m a web designer</p>
+        <p>&amp; developer</p>                
       </div>
     );
   }
