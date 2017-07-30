eef0694be99d087f8af8402ae411232334402f5a
Nessa <nessa.develops@gmail.com>
2017-07-30 10:33:00 -0500

    Cleaned up + added css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,9 +6,9 @@ import '../../styles/theme.css';
 class Home extends Component {
   render() {
     return (
-      <div className="App Montserrat">
+      <div className="App">
         <p>hi my name is</p>
-        <h1 className="MontserratBold">nessa.</h1>
+        <h1>nessa.</h1>
         <p>i&#39;m a web designer</p>
         <p>&amp; developer</p>                
       </div>
