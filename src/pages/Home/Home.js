05c18d66e98c9943c3ff97f64ba84cdcc5d908a0
Nessa <nessa.develops@gmail.com>
2017-07-31 18:35:29 -0500

    Text fade in entrance

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,13 +7,22 @@ import '../../styles/theme.css';
 class Home extends Component {
   
   componentDidMount() {
-    var myAnimation = anime({
+    var hiAnimate = anime({
       targets: ['.hi'],
       translateX: [-310, 15],
       delay: 800,
       opacity: {
         value: [0, 1],
         duration: 100,
+      },
+    });
+
+    var textAnimate = anime({
+      targets: ['.name, .smol, .job'],
+      delay: 1200,
+      opacity: {
+        value: [0, 1],
+        duration: 10000,
       },
     });
   }
@@ -25,7 +34,7 @@ class Home extends Component {
         <div className="wrapper">  
           <span><p className="hi" data-duration="2000">hi</p> my name is</span>
           <h1 className="name">nessa.</h1>
-          <p>&amp; i&#39;m a full stack web developer</p>
+          <p className="job">&amp; i&#39;m a full stack web developer</p>
           <p className="smol">(plus i really enjoy design)</p>                
         </div>
       </div>
