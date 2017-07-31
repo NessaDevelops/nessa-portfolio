7eeb0b023eb073c8442461726e6152ac30ab4531
Nessa <nessa.develops@gmail.com>
2017-07-31 18:22:45 -0500

    Added animejs + hi animate

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import anime from 'animejs';
 import NavBar from '../../components/NavBar';
 import './Home.css';
 import '../../styles/theme.css';
 
 class Home extends Component {
+  
+  componentDidMount() {
+    var myAnimation = anime({
+      targets: ['.hi'],
+      translateX: [-310, 15],
+      delay: 800,
+      opacity: {
+        value: [0, 1],
+        duration: 100,
+      },
+    });
+  }
+
   render() {
     return (
       <div className="container">
         <NavBar></NavBar>
         <div className="wrapper">  
-          <p>hi my name is</p>
+          <span><p className="hi" data-duration="2000">hi</p> my name is</span>
           <h1 className="name">nessa.</h1>
           <p>&amp; i&#39;m a full stack web developer</p>
           <p className="smol">(plus i really enjoy design)</p>                
