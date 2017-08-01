e3eba85032b332996765585ffccaa2307c3045c3
Nessa <nessa.develops@gmail.com>
2017-08-01 13:55:53 -0500

    Add form + fixed form styling

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import './Contact.css';
+import NavBar from '../../components/NavBar';
+import ValidateForm from './form';
 
 class Contact extends Component {
+
+  componentDidMount() {
+    // ValidateForm();
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>CONTACT</h1>
+      <div className="content">
+        <NavBar></NavBar>
+        <div className="form-wrapper">
+          <h2>Enjoy my work?<br/>Send me an email if you&#39;d like to hire me, collaborate, or have any questions.</h2>
+          <form method="POST" id="contactForm" name="contactform" action="contact-form-handler.php" onsubmit="return validateForm()">
+            <input type="text" id="name" name="name" placeholder="Name"></input>
+            <input type="text" id="email" name="email" placeholder="Email"></input>
+            <textarea id="message" name="message" placeholder="Message"></textarea>
+            <input type="submit" value="Submit" />
+          </form>
+          <a href="https://www.linkedin.com/in/jenessa-stabler"><div className="linkedin"></div></a>
+        </div>
       </div>
     );
   }
