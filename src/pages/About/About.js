2682393cc7962d8a8b574636e67941a8f122651f
NessaDevelops <nessa.develops@gmail.com>
2017-08-14 11:33:57 -0500

    About filler content

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import NavBar from '../../components/NavBar';
 import './About.css';
 
 class About extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>ABOUT</h1>
+      <div className="container">
+        <NavBar></NavBar>
+        <div className="wrapper">
+          <h2>About Me</h2>
+        </div>
       </div>
-    );
+     )
   }
-}
+};
 
 export default About;
