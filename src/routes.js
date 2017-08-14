4db96a284698c399b229069679c52f7fdd51d769
NessaDevelops <nessa.develops@gmail.com>
2017-08-14 11:32:57 -0500

    Added projects to route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Projects from './pages/Projects/Projects';
 import Contact from './pages/Contact/Contact.jsx';
 import NotFound from './pages/404Page/404Page';
 
@@ -11,8 +12,9 @@ class Routes extends Component {
       <BrowserRouter>
         <div>
           <Route exact path='/' component={Home} />
-          <Route path='/contact' component={Contact} />
           <Route path='/about' component={About} />
+          <Route path='/projects' component={Projects} />          
+          <Route path='/contact' component={Contact} />
           <Route path='/404' component={NotFound} />
         </div>
       </BrowserRouter>
