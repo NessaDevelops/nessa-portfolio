bc10a6f710f97ce07c8c8b95a1fa30f525a9a6b3
NessaDevelops <nessa.develops@gmail.com>
2017-08-15 13:08:33 -0500

    Modal style refactor

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -18,31 +18,10 @@ class Modal extends Component {
     if (this.props.isOpen === false)
       return null
 
-    let modalStyle = {
-      position: 'absolute',
-      height: '80%',
-      width: '80%',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      zIndex: '9999',
-      background: '#fff'
-    }
-
-    let backdropStyle = {
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      top: '0px',
-      left: '0px',
-      zIndex: '9998',
-      background: 'rgba(0, 0, 0, 0.3)'
-    }
-
     return (
       <div>
-        <div style={modalStyle}>{this.props.children}</div>
-        <div style={backdropStyle} onClick={e => this.close(e)}/>}
+        <div className='modalStyle'>{this.props.children}</div>
+        <div className='backdropStyle' onClick={e => this.close(e)}/>}
       </div>
     )
   }
