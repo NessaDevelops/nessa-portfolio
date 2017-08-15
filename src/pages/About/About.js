dce811a030ff5194092d64e1495bd97e586830cf
NessaDevelops <nessa.develops@gmail.com>
2017-08-15 17:23:48 -0500

    about me quick c/p from old website

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -5,10 +5,33 @@ import './About.css';
 class About extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="about-container">
         <NavBar></NavBar>
         <div className="wrapper">
           <h2>About Me</h2>
+          <div className="content">
+            <div className="me"></div>
+            <div className="faq">
+              <h3>Years young:</h3>
+              <p>26</p>
+              <h3>Living in:</h3>
+              <p>Houston, Texas</p>
+              <h3>Front-end development:</h3>
+              <p>HTML5, CSS3, JavaScript, Angular, React, Redux</p>
+              <h3>Back-end development:</h3>
+              <p>Node, MongoDB</p>
+              <h3>Design:</h3>
+              <p>Illustrator, Photoshop, InDesign</p>
+              <h3>Additional skills:</h3>
+              <p>Responsive websites, animation, performance</p>
+            </div>
+            <div className="bio">
+              <a href="https://www.linkedin.com/in/jenessa-stabler/">LinkedIn</a>
+              <a href="https://github.com/NessaDevelops">GitHub</a>
+              <a href="https://codepen.io/NessaDevelops/">CodePen</a>
+              <a href="https://dribbble.com/NessaDevelops">Dribbble</a>
+            </div>
+          </div>
         </div>
       </div>
      )
