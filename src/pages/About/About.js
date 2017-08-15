439979ba7c7227bdf4725244da683cb75ffed6d3
NessaDevelops <nessa.develops@gmail.com>
2017-08-15 17:41:14 -0500

    about me pg fix up

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -8,21 +8,23 @@ class About extends Component {
       <div className="about-container">
         <NavBar></NavBar>
         <div className="wrapper">
-          <h2>About Me</h2>
+          <h2>I create modern &amp; clean websites.</h2>
+          <h2>:]</h2>
           <div className="content">
             <div className="me"></div>
             <div className="faq">
-              <h3>Years young:</h3>
+              <h3>FAQ</h3>
+              <h3>Years Young:</h3>
               <p>26</p>
-              <h3>Living in:</h3>
+              <h3>Living In:</h3>
               <p>Houston, Texas</p>
-              <h3>Front-end development:</h3>
+              <h3>Front-end Development:</h3>
               <p>HTML5, CSS3, JavaScript, Angular, React, Redux</p>
-              <h3>Back-end development:</h3>
+              <h3>Back-end Development:</h3>
               <p>Node, MongoDB</p>
               <h3>Design:</h3>
               <p>Illustrator, Photoshop, InDesign</p>
-              <h3>Additional skills:</h3>
+              <h3>Additional Skills:</h3>
               <p>Responsive websites, animation, performance</p>
             </div>
             <div className="bio">
