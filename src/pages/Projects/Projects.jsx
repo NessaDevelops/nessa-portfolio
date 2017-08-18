6f8310df630815ecf90a397a4c89cadd5a487ba2
NessaDevelops <nessa.develops@gmail.com>
2017-08-18 14:29:38 -0500

    Project styling + project content

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -41,9 +41,13 @@ class Tile extends Component {
               <p>{this.props.role}</p>
               <a href={this.props.github}>GitHub</a>
               <Carousel imgs={this.props.imgs}></Carousel>
-              <div className="desc">
-                <h3>Project Description</h3>
-                <p>{this.props.desc}</p>
+              <div className="summ">
+                <h3>Project Summary</h3>
+                <p>{this.props.summ}</p>
+              </div>
+              <div className="process">
+                <h3>The Process</h3>
+                <p>{this.props.process}</p>
               </div>
               <div className="tech">
                 <h3>Technologies Involved</h3>
@@ -62,7 +66,8 @@ Tile.propTypes = {
   role: React.PropTypes.string.isRequired,
   github: React.PropTypes.string.isRequired,
   imgs: React.PropTypes.arrayOf(String),
-  desc: React.PropTypes.string.isRequired,
+  summ: React.PropTypes.string.isRequired,
+  process: React.PropTypes.string.isRequired,
   tech: React.PropTypes.string.isRequired
 }
 
@@ -71,8 +76,9 @@ class TileList extends Component {
     super(props);
     this.state = {
       tiles: [
-        { imgSrc: "/static/media/home.fdaef3db.png", title: "A Drop in the Bucket", role: "UX/UI Design", github: "https://github.com/NessaDevelops/A-Drop-in-the-Bucket",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sem lectus, lacinia eget libero non, bibendum tincidunt justo. Duis diam turpis, efficitur id euismod porttitor, pretium hendrerit velit. Nam eu eleifend augue. Suspendisse interdum est a velit finibus, sed aliquam tellus finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean feugiat velit pharetra tempus tempus. Sed pharetra non erat nec rhoncus. Cras nec turpis blandit, pulvinar urna eget, luctus nisi.",
+        { imgSrc: "/static/media/home.fdaef3db.png", title: "A Drop in the Bucket", role: "Front-end / Back-end / UX+UI Design", github: "https://github.com/NessaDevelops/A-Drop-in-the-Bucket",
+          summ: "A Drop in the Bucket is a web application that allows a user to create lists that goals are added to. These goals are aspirations that the user wants to accomplish in their lifetime. A list can either be made for solo use or completed with others. After completing a goal it is then stored as a memory where the user can leave a description & pictures to remember the event.",
+          process: "I designed & programmed the web application in a total of 8 weeks while learning MEAN stack in the process. The designs were crafted using Adobe Illustrator, Indesign, & Photoshop. The front-end was done with Angular as I wanted to make it a SPA. The back-end was Node/Express with MongoDB for the database. This project included user accounts & a map API using OpenLayers.  This allowed the user to either create a custom goal or select premade goals based on location from a map. When the goal is completed the user is able to turn the goal into a memory with a description & photos - allowing them to keep a memory album of the goals they have finished.",
           tech: "Angular, Express, Node, MongoDB, JQuery, Mongoose, Nodemon, SASS, InDesign, PhotoShop, Illustrator, Grunt",
           imgs: [ 
             "/static/media/home.fdaef3db.png", 
@@ -88,7 +94,8 @@ class TileList extends Component {
             "/static/media/achievement.30d20648.png"
           ] },
         { imgSrc: "/static/media/splash.f923149e.png", title: "Primary Services", role: "UX/UI Design", github: "",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sem lectus, lacinia eget libero non, bibendum tincidunt justo. Duis diam turpis, efficitur id euismod porttitor, pretium hendrerit velit. Nam eu eleifend augue. Suspendisse interdum est a velit finibus, sed aliquam tellus finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean feugiat velit pharetra tempus tempus. Sed pharetra non erat nec rhoncus. Cras nec turpis blandit, pulvinar urna eget, luctus nisi.",
+          summ: "Primary Services contacted me to work on redesigning & implementing a new way to search & view jobs on their current website. Their job search linked to Job Diva's web application & they wanted to have their own job search located on their website.",
+          process: "I researched UI & UX practices used by other successful job recruitment company websites & showed my findings to the team I was working with at Primary Services. I crafted mockups using Illustrator & InDesign. We had meetings to discuss the process & I made revisions based on the feedback. I wanted to keep the color scheme & overall feel of the current website while giving the new designs a more modern feel that will also mesh if Primary Services were to modernized their website design in the future.",
           tech: "Illustrator, InDesign",
           imgs: [ 
             "/static/media/splash.f923149e.png", 
@@ -96,7 +103,8 @@ class TileList extends Component {
           ]
         },
         { imgSrc: "/static/media/signup1.872c46ec.png", title: "Chronicled", role: "UX/UI Design", github: "https://github.com/chronicled/partner-portal-web",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sem lectus, lacinia eget libero non, bibendum tincidunt justo. Duis diam turpis, efficitur id euismod porttitor, pretium hendrerit velit. Nam eu eleifend augue. Suspendisse interdum est a velit finibus, sed aliquam tellus finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean feugiat velit pharetra tempus tempus. Sed pharetra non erat nec rhoncus. Cras nec turpis blandit, pulvinar urna eget, luctus nisi.", 
+          summ: "",
+          process: "",          
           tech: "React, ES6, Redux, Webpack",
           imgs: [
             "/static/media/signup1.872c46ec.png",
@@ -105,14 +113,16 @@ class TileList extends Component {
           ]
         },
         { imgSrc: "/static/media/pugger.4e2f6cd8.png", title: "Pugger", role: "UX/UI Design", github: "https://github.com/NessaDevelops/Pugger",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sem lectus, lacinia eget libero non, bibendum tincidunt justo. Duis diam turpis, efficitur id euismod porttitor, pretium hendrerit velit. Nam eu eleifend augue. Suspendisse interdum est a velit finibus, sed aliquam tellus finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean feugiat velit pharetra tempus tempus. Sed pharetra non erat nec rhoncus. Cras nec turpis blandit, pulvinar urna eget, luctus nisi.", 
+          summ: "",
+          process: "",          
           tech: "JavaScript, CreateJS, Illustrator",
           imgs: [
             "/static/media/pugger.4e2f6cd8.png"
           ]
         },
         { imgSrc: "/static/media/nugalega.66ff7183.png", title: "NuGalega", role: "UX/UI Design", github: "https://github.com/erob2620/NUGalega",
-          desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean sem lectus, lacinia eget libero non, bibendum tincidunt justo. Duis diam turpis, efficitur id euismod porttitor, pretium hendrerit velit. Nam eu eleifend augue. Suspendisse interdum est a velit finibus, sed aliquam tellus finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean feugiat velit pharetra tempus tempus. Sed pharetra non erat nec rhoncus. Cras nec turpis blandit, pulvinar urna eget, luctus nisi.", 
+          summ: "",
+          process: "",          
           tech: "JavaScript, CreateJS",
           imgs: [
             "/static/media/nugalega.66ff7183.png"
@@ -123,7 +133,7 @@ class TileList extends Component {
   }
   render() {
     const tileList = this.state.tiles.map((tile, index) =>
-      <Tile key={index} src={tile.imgSrc} title={tile.title} role={tile.role} github={tile.github} imgs={tile.imgs} desc={tile.desc} tech={tile.tech}></Tile>
+      <Tile key={index} src={tile.imgSrc} title={tile.title} role={tile.role} github={tile.github} imgs={tile.imgs} summ={tile.summ} process={tile.process} tech={tile.tech}></Tile>
     );
     return (
       <div className="tiles">{tileList}</div>
